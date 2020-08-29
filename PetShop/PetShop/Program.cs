d150496a3da4772fbbaa510b4207fab7c1f5e1f4
Jonas Spatzek <Jonas_spatzek@hotmail.com>
2020-08-29 19:42:29 +0200

    Revert "petshop until whileloop"
    This reverts commit 4da8ee8052f22675109d384c0f37ac970506e1db.

--- PetShop/PetShop/Program.cs
+++ PetShop/PetShop/Program.cs
@@ -6,27 +6,7 @@ namespace PetShop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Type your name");
-
-            string name = Console.ReadLine();
-
-            var nameContainsJ = name.ToLower().Contains("j");
-
-            Console.WriteLine("Hi " + name);
-            if (nameContainsJ)
-            { 
-                Console.WriteLine("Your name Contains J");
-            }
-            else
-            {
-                Console.WriteLine("Your name does not contain J");
-            }
-         
-
-            Console.WriteLine("Press any key to exit...");
+            Console.WriteLine("Hello World!");
         }
     }
 }
-
-
-
