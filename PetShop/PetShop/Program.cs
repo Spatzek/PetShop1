4da8ee8052f22675109d384c0f37ac970506e1db
Jonas Spatzek <Jonas_spatzek@hotmail.com>
2020-08-29 19:41:15 +0200

    petshop until whileloop

--- PetShop/PetShop/Program.cs
+++ PetShop/PetShop/Program.cs
@@ -6,7 +6,27 @@ namespace PetShop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Type your name");
+
+            string name = Console.ReadLine();
+
+            var nameContainsJ = name.ToLower().Contains("j");
+
+            Console.WriteLine("Hi " + name);
+            if (nameContainsJ)
+            { 
+                Console.WriteLine("Your name Contains J");
+            }
+            else
+            {
+                Console.WriteLine("Your name does not contain J");
+            }
+         
+
+            Console.WriteLine("Press any key to exit...");
         }
     }
 }
+
+
+
