c51e44dd93a1bb1d0dd18679836af92960165d10
Jonas Spatzek <Jonas_spatzek@hotmail.com>
2020-09-02 10:28:39 +0200

    Listing

--- petShop2/petShop2/Program.cs
+++ petShop2/petShop2/Program.cs
@@ -11,13 +11,40 @@ namespace petShop2
     {
 
         static void Main(string[] args)
+
         {
-            FakeDB.initData();
-            IPetRepository petRepository = new PetRepository();
-            IPetService petService = new PetService(petRepository);
-            Printer printer = new Printer(petService);
-            printer.StartUi();        
+            string[] menuItems = {
+                "list All customers",
+                "Add Customer",
+                "Delete Customer",
+                "Edit Customer",
+                "Exit",
+            };
+            ShowMenu(menuItems);
+            Console.ReadLine();
         }
 
+        private static int ShowMenu(string[] menuItems)
+        {
+            Console.Clear();
+            Console.WriteLine("Select an option");
+            Console.WriteLine("");
+
+
+
+            for (int i = 1; i < menuItems.Length; i++)
+            {
+                Console.WriteLine(i + ":" + menuItems[i]);
+            }
+            int selection;
+            while (!int.TryParse(Console.ReadLine(), out selection)
+                || selection < 1
+                || selection >5)
+            {
+                Console.WriteLine("Select a number between 1-5");
+            }
+            Console.WriteLine("Selection " + selection);
+            return selection;
+        }
     }
 }
