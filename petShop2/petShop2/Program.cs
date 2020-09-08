4a9f95b357a47a2e4a590bf2a2b54f243c8c1101
Jonas Spatzek <Jonas_spatzek@hotmail.com>
2020-09-08 14:52:05 +0200

    Pet id

--- petShop2/petShop2/Program.cs
+++ petShop2/petShop2/Program.cs
@@ -13,15 +13,41 @@ namespace petShop2
 {
     class Program
     {
+        static int petId = 1;
+        static List<Pet> pets = new List<Pet>();
 
         static void Main(string[] args)
 
         {
+
+            var pet1 = new Pet()
+            {
+                PetId = petId++,
+                Name = "Mickey",
+                Color = "red",
+                Race = "Cat",
+                PreviousOwner = "Bob Dylan",
+                Price = 100,
+            };
+            pets.Add(pet1);
+
+            pets.Add(new Pet()
+            {
+                PetId = petId++,
+                Name = "Peter",
+                Color = "Black",
+                Race = "Dog",
+                PreviousOwner = "Man Dylan",
+                Price = 150,
+            });
+
+            
+
             string[] menuItems = {
                 "list All pets",
                 "Show Pets",
-                "Search race",
                 "Add Pet",
+                "Search Race",
                 "Delete Pet",
                 "Edit Pet",
                 "Sort Pets",
@@ -30,17 +56,18 @@ namespace petShop2
             };
 
             var selection = ShowMenu(menuItems);
-                while (selection != 1)
+                while (selection != 9)
             {
                 switch(selection)
                 {
                     case 1:
                         ShowPets();
                         break;
-                        /**
+                        
                     case 2:
-                        SearchPets();
+                        AddPets();
                         break;
+                        /*
                     case 3:
                         AddPet();
                         break;
@@ -64,18 +91,73 @@ namespace petShop2
                 selection = ShowMenu(menuItems);
 
             }
-            
-            Console.WriteLine("");
+            Console.WriteLine("closeing program");
+           
             Console.ReadLine(); 
-            Console.Clear();
+            
         }
 
+        private static void AddPets()
+        {
+            Console.WriteLine("Pet Id: ");
+            int PetId = petId++;
 
+            Console.WriteLine("Name: ");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Race: ");
+            var race = Console.ReadLine();
+
+            Console.WriteLine("Color: ");
+            var color = Console.ReadLine();
+
+            Console.WriteLine("PreviousOwner: ");
+            var previousOwner = Console.ReadLine();
+
+            Console.WriteLine("Price: ");
+            var price = Console.ReadLine();
+
+            pets.Add(new Pet()
+            {
+                PetId = PetId++,
+                Name = name,
+                Race = race,
+                Color = color,
+                PreviousOwner = previousOwner,
+                //Price = double price,
+
+            });
+        }
+
+        private static string GetPetId()
+        {
+            return Console.ReadLine();
+        }
+
+        private static void ShowPets()
+        {
+            
+            foreach (var Pet in pets)
+            {
+                Console.WriteLine($"PetId: {Pet.PetId}");
+                Console.WriteLine($"Name: {Pet.Name }");
+                Console.WriteLine($"Race: {Pet.Race}");
+                Console.WriteLine($"Color: {Pet.Color }");
+                Console.WriteLine($"PreviousOwner: {Pet.PreviousOwner}");
+                Console.WriteLine($"Price: {Pet.Price }");
+                Console.WriteLine("\n");
+
+            }
+            Console.WriteLine("\n");
+        }
         public static int ShowMenu(string[] menuItems)
         {
+          
+            Console.WriteLine("Select an option:\n");
+            
             for (int i = 1; i < menuItems.Length; i++)
             {
-                Console.WriteLine($"{(i + 0)}: {menuItems[i]}");
+                Console.WriteLine($"{(i + 0)} :{menuItems[i]}");
             }
             int selection;
             while (!int.TryParse(Console.ReadLine(), out selection)
@@ -84,18 +166,10 @@ namespace petShop2
             {
                 Console.WriteLine("Select a number between 1-8");
             }
-            Console.WriteLine("Selected " + selection);
+           
             return selection;
         }
-        private static void ShowPets()
-        {
-            Console.WriteLine();
-            foreach (var pet in FakeDB.pets)
-            {
-                Console.WriteLine(FakeDB.pets);
-            }
-            Console.WriteLine();
-        }
+
 
 
     }
